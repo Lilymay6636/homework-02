d6107a48bbc3f45c7b24ffba0d485ee8adec5296
Lilymay6636 <lily@lilymay.idv.tw>
2017-08-30 03:09:56 +0800

    第二週作業

---
+++ hw06.js
@@ -0,0 +1,22 @@
+function random(m) {
+    var m = 2016;
+    return Math.floor(Math.random() * m + 1);
+}
+
+function max(a, b) {
+    var a = 20;
+    var b = 19;
+    return Math.max(a, b);
+}
+
+function lastChar(arr1) {
+    var arr1 = [2, 42, 66];
+    return arr1[arr1.length - 1];
+
+}
+
+function squares(a, b) {
+    var a = 2;
+    var b = 5;
+    return Math.pow(a, 2) + Math.pow(b, 2);
+}
