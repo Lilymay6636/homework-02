5f3c580d42e04d394fc9c24920917e0c2db29229
Lilymay6636 <lily@lilymay.idv.tw>
2017-09-08 05:14:17 +0800

    第二週作業

--- hw06.js
+++ hw06.js
@@ -1,22 +1,26 @@
 function random(m) {
-    var m = 2016;
     return Math.floor(Math.random() * m + 1);
 }
 
 function max(a, b) {
-    var a = 20;
-    var b = 19;
     return Math.max(a, b);
 }
 
-function lastChar(arr1) {
-    var arr1 = [2, 42, 66];
-    return arr1[arr1.length - 1];
+function lastChar(arr) {
+    var arr=[2, 42, 66]
+    return arr[arr.length - 1];
 
 }
 
 function squares(a, b) {
-    var a = 2;
-    var b = 5;
     return Math.pow(a, 2) + Math.pow(b, 2);
+}
+
+
+function leapYear(y){
+ var yy= y%4===0 && y%100!=0 ;
+    if (yy || y%400===0){
+        return "Yes";
+    }
+    else{return "NO";}
 }
